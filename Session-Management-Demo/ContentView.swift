74ff772cd6ac4d8c395ff04bfe7bc5b24eb8be93
Richa Kalani <richakalani07@gmail.com>
2024-06-23 16:06:44 +0530

    Initial Commit

---
+++ Session-Management-Demo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Session-Management-Demo
+//
+//  Created by Richa Kalani on 23/06/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Session-Management-Demo/Session_Management_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Session_Management_DemoApp.swift
+//  Session-Management-Demo
+//
+//  Created by Richa Kalani on 23/06/24.
+//
+
+import SwiftUI
+
+@main
+struct Session_Management_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
