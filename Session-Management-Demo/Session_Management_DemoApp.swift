4cfdac2cffae41c95db50f9ab493454d40ea316b
Richa Kalani <richakalani07@gmail.com>
2024-06-23 17:16:25 +0530

    initial set up

--- Session-Management-Demo/Session_Management_DemoApp.swift
+++ Session-Management-Demo/Session_Management_DemoApp.swift
@@ -6,9 +6,21 @@
 //
 
 import SwiftUI
+import FirebaseCore
+import FirebaseFirestore
+import FirebaseAuth
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        return true
+    }
+}
 
 @main
 struct Session_Management_DemoApp: App {
+    // register app delegate for firebase
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     var body: some Scene {
         WindowGroup {
             ContentView()
